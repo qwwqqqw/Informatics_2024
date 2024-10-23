37142644ef61a90f4facf7d46546e88e1f9a3ecf
unknown <pshenichnikov.6002@gmail.com>
2024-10-23 15:14:04 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "isuct.ru/informatics2022/labs"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/labs"
+)
 
 func main() {
+	fmt.Println("Пшеничников Иван Михайлович")
 	labs.Runlab4()
+	labs.Runlab6()
 }
